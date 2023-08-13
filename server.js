b48262767598f517f3f4f8a17a4f5e900c4d6c39
Hiago Souza <hiagofss98@gmail.com>
2023-08-13 15:25:10 -0300

    Working with routes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require("http");
 const PORT = 3000;
 
+const routes = {
+  "/": "Course of NodeJs",
+  "/books": "List of books",
+  "/authors": "List of authors",
+  "/editor": "List of editors",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
 
-  res.end("Hello World");
+  res.end(routes[req.url] || "404 - Not Found");
 });
 
 server.listen(PORT, () => {
