507dd0e8c4f39a8ec4d78b38179d28dcb3f49da7
Hiago Souza <hiagofss98@gmail.com>
2023-08-13 20:59:53 -0300

    Create new Book and add in array of books

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const books = [
   { id: 1, title: "Harry Potter", author: "J. K. Rowling" },
   { id: 2, title: "Lord of the Rings", author: "J. R. R. Tolkien" },
@@ -10,8 +12,15 @@ const books = [
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
+
 app.get("/books", (req, res) => {
   res.status(200).json(books);
+});
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+  books.push(book);
+  res.status(201).send("Book added to the database");
 });
 
 export default app;
