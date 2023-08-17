f1a73116e21e3ce176337b5475cb72af19d05428
Hiago Souza <hiagofss98@gmail.com>
2023-08-16 21:29:11 -0300

    Implements route to get Book

--- src/app.js
+++ src/app.js
@@ -17,10 +17,29 @@ app.get("/books", (req, res) => {
   res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+  const { id } = req.params;
+
+  const book = findBook(Number(id));
+
+  if (!book) {
+    return res.status(404).send("Book not found");
+  }
+
+  res.status(200).json(book);
+});
+
 app.post("/books", (req, res) => {
   const book = req.body;
   books.push(book);
   res.status(201).send("Book added to the database");
 });
+
+function findBook(id) {
+  console.log(typeof id);
+  books.forEach((book) => console.log(typeof book.id));
+
+  return books.find((book) => book.id === id);
+}
 
 export default app;
