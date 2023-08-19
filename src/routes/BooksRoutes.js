0f5dda3cde61369d2ecb2cbeb1dd097c17d0ac27
Hiago Souza <hiagofss98@gmail.com>
2023-08-19 14:44:57 -0300

    Remove unused code

--- src/routes/BooksRoutes.js
+++ src/routes/BooksRoutes.js
@@ -10,40 +10,4 @@ router
   .get("/books/:id", BooksController.getBookById)
   .delete("/books/:id", BooksController.deleteBookById);
 
-// router.get("/books", (req, res) => {
-//   res.status(200).json(books);
-// });
-
-// router.get("/books/:id", (req, res) => {
-//   const id = Number(req.params.id);
-
-//   const book = findBook(id);
-
-//   if (!book) {
-//     return res.status(404).send("Book not found");
-//   }
-
-//   res.status(200).json(book);
-// });
-
-// router.put("/books/:id", (req, res) => {
-//   const id = Number(req.params.id);
-
-//   const book = findBook(id);
-
-//   console.log(book);
-
-//   if (!book) {
-//     return res.status(404).send("Book not found");
-//   }
-
-//   const { title, author } = req.body;
-
-//   const bookInfoUpdate = { id, title, author };
-
-//   books[books.indexOf(book)] = bookInfoUpdate;
-
-//   res.status(200).send("Book updated successfully");
-// });
-
 export default router;
